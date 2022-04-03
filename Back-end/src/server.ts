b9827159d54b393812c737318ccd12d99aef97c3
XMoSalahX <01154615199>
2022-04-03 03:45:45 +0200

    Add route to our server

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-
+import route from "./Route/route";
 const app: express.Application = express();
 const address: string = "http://localhost:8003";
 
@@ -8,5 +8,10 @@ app.use(express.json());
 app.listen(8003, function () {
   console.log(`starting app on: ${address}`);
 });
+
+app.use(express.json());
+
+//Route app to route file
+route(app);
 
 export default app;
