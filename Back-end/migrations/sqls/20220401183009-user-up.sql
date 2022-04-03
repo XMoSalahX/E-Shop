d8660092920ee8faa52a59531571a5ec7115d572
XMoSalahX <01154615199>
2022-04-03 03:44:56 +0200

    Add create table command for user table

--- Back-end/migrations/sqls/20220401183009-user-up.sql
+++ Back-end/migrations/sqls/20220401183009-user-up.sql
@@ -1,1 +1,1 @@
-/* Replace with your SQL commands */
+CREATE TABLE user_shop(id SERIAL PRIMARY KEY,first_name VARCHAR(50) NOT NULL,last_name VARCHAR(50) NOT NULL ,email VARCHAR(255) NOT NULL,password VARCHAR(255) NOT NULL,responsibility VARCHAR(50),status VARCHAR(50));
