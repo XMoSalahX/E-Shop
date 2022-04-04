b016c276b1d4837e21fd3d23f60dda58c1cce192
XMoSalahX <01154615199>
2022-04-05 01:37:38 +0200

    Handeling msg to user

---
+++ Back-end/src/utlities/mail controler.ts
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+import setMsg from "../template/verify email";
+
+const { EMAIL, EMAIL_PASS } = process.env;
+
+// Email verification
+const mailVer = async (email: string, id: string, target: string) => {
+  let temp;
+  if (target == "verify") {
+    temp = setMsg(id);
+  } else if (target == "forget") {
+    temp = setMsg(id);
+  }
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: EMAIL,
+      pass: EMAIL_PASS,
+    },
+  });
+
+  // send mail with defined transport object
+  await transporter.sendMail({
+    from: EMAIL, // sender address
+    to: email, // list of receivers
+    subject: "E-Commerce Verification Message", // Subject line
+    html: temp, // html body
+  });
+};
+
+export default mailVer;
