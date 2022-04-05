724c254596e48ad2153196c6bafa42580c13dd9b
XMoSalahX <01154615199>
2022-04-05 16:11:06 +0200

    Add swagger function to Route file

--- Back-end/src/Route/route.ts
+++ Back-end/src/Route/route.ts
@@ -1,9 +1,11 @@
 import userEndPoint from "../handeler/user_handerler";
 import { Application } from "express";
+import swagger from "../../documentation/swagger doc";
 
 // Route function to hold all endpoint
 const route = (app: Application) => {
   userEndPoint(app);
+  swagger(app);
 };
 
 export default route;
