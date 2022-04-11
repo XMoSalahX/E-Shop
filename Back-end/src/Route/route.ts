8f97d0a90a23cfed5321c1ffc66286cb67240859
XMoSalahX <01154615199>
2022-04-11 20:02:22 +0200

    Add seller endpoint to route file

--- Back-end/src/Route/route.ts
+++ Back-end/src/Route/route.ts
@@ -1,11 +1,13 @@
 import userEndPoint from "../handeler/user_handerler";
 import { Application } from "express";
 import swagger from "../../documentation/swagger doc";
+import sellerEndpoint from "../handeler/Seller Control handeler";
 
 // Route function to hold all endpoint
 const route = (app: Application) => {
   userEndPoint(app);
   swagger(app);
+  sellerEndpoint(app);
 };
 
 export default route;
