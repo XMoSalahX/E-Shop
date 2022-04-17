039eb4fc7defd55765ddcd981b6ea48d2a69f654
XMoSalahX <01154615199>
2022-04-17 15:09:40 +0200

    Add puplic middleware

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -5,13 +5,12 @@ const app: express.Application = express();
 const address: string = "http://localhost:8003";
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("upload"));
 
 app.listen(8003, function () {
   console.log(`starting app on: ${address}`);
 });
-
-app.use(express.json());
-
 //Route app to route file
 route(app);
 
